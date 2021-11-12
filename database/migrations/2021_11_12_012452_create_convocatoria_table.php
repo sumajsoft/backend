46c968fac52ff07bf73d277f0002aa99eb8f0bef
mauCZ <madavaing@gmail.com>
2021-11-11 22:06:04 -0400

    controller y model de Convocatoria creados y ruta de api .../api/convocatorias

---
+++ database/migrations/2021_11_12_012452_create_convocatoria_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConvocatoriaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('convocatoria', function (Blueprint $table) {
+            $table->id();
+            $table->string("titulo",30);
+            $table->string("codigo",30);
+            $table->string("semestre",1);
+            $table->string("gestion",4);
+            $table->string("archivo",50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('convocatoria');
+    }
+}
