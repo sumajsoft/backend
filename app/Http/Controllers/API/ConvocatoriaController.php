06f04be203c892cd144fd0f8ab5ea8befa699ef6
mauCZ <madavaing@gmail.com>
2021-11-12 21:51:41 -0400

    rutas de api para POST y PUT de Convocatoria anhadidos

--- app/Http/Controllers/API/ConvocatoriaController.php
+++ app/Http/Controllers/API/ConvocatoriaController.php
@@ -23,9 +23,17 @@ class ConvocatoriaController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        //
+    public function store(Request $request){
+      $nuevaConv = new Convocatoria();
+      $nuevaConv->titulo = $request->titulo;
+      $nuevaConv->codigo = $request->codigo;
+      $nuevaConv->semestre = $request->semestre;
+      $nuevaConv->gestion = $request->gestion;
+      $nuevaConv->archivo = $request->archivo;
+      $nuevaConv->save();
+      return response()->json([
+        'sePudo' => True
+      ]);
     }
 
     /**
@@ -46,9 +54,17 @@ class ConvocatoriaController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-    {
-        //
+    public function update(Request $request, $id){
+      $conv = Convocatoria::find($id);
+      $conv->titulo = $request->titulo;
+      $conv->codigo = $request->codigo;
+      $conv->semestre = $request->semestre;
+      $conv->gestion = $request->gestion;
+      $conv->archivo = $request->archivo;
+      $conv->save();
+      return response()->json([
+        "sePudo" => True
+      ]);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/convocatorias', [ConvocatoriaController::class,'index']);
+Route::get('convocatorias', [ConvocatoriaController::class,'index']);
+
+Route::post('convocatorias',[ConvocatoriaController::class,'store']);
+
+Route::put('convocatorias/{id}', [ConvocatoriaController::class,'store']);
